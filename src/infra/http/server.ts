be7f99b078520e88bd703a575ab7b476d2c6c690
Rafael Rocha <rafaelgus3000@gmail.com>
2025-11-28 20:46:09 -0300

    feat: iniciar servidor http e rota de teste

---
+++ src/infra/http/server.ts
@@ -0,0 +1,21 @@
+import express, { type Express, type Router } from 'express'
+
+export class ServerHTTP {
+  private app: Express
+
+  private constructor(routes: Router) {
+    this.app = express()
+    this.app.use(express.json())
+    this.app.use(routes)
+  }
+
+  public static create(routes: Router) {
+    return new ServerHTTP(routes)
+  }
+
+  public start() {
+    this.app.listen(process.env.PORT || 3000, () => {
+      console.log(`🖥️  Server running in port ${process.env.PORT}`)
+    })
+  }
+}

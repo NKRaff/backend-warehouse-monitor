fa9b634e26f89e513fa4b399033463fecdc05dc5
Rafael Rocha <rafaelgus3000@gmail.com>
2025-11-30 19:34:52 -0300

    feat: adiciona outros metodos padrões ao repository

--- src/infra/database/ambiente/ambiente.repository.ts
+++ src/infra/database/ambiente/ambiente.repository.ts
@@ -1,4 +1,4 @@
-import { Ambiente } from '@/domain/ambiente/ambiente.entity.js'
+import { Ambiente, type AmbienteProps } from '@/domain/ambiente/ambiente.entity.js'
 import type { AmbienteRepository } from '@/domain/ambiente/ambiente.repository.js'
 import { AmbienteModel } from './ambiente.model.js'
 
@@ -10,19 +10,42 @@ export class MongooseAmbienteRepository implements AmbienteRepository {
   }
 
   public async save(ambiente: Ambiente): Promise<void> {
-    const data = {
+    await AmbienteModel.create({
       _id: ambiente.id,
       nome: ambiente.nome,
       tipo: ambiente.tipo,
       descricao: ambiente.descricao,
-    }
-
-    const ambienteDoc = new AmbienteModel(data)
-    await ambienteDoc.save()
+    })
   }
 
   public async findAll(): Promise<Ambiente[]> {
-    const ambientesDoc = await AmbienteModel.find()
+    const ambientesDoc = await AmbienteModel.find().lean<AmbienteProps[]>()
     return ambientesDoc.map((doc) => Ambiente.create(doc.id, doc.nome, doc.tipo, doc.descricao))
+  }
+
+  public async findById(id: string): Promise<Ambiente> {
+    const ambienteDoc = await AmbienteModel.findById(id).lean<AmbienteProps>()
+    if (!ambienteDoc) throw new Error('Nenhum ambiente com esse Id foi encontrado.')
+    return Ambiente.create(
+      ambienteDoc.id,
+      ambienteDoc.nome,
+      ambienteDoc.tipo,
+      ambienteDoc.descricao,
+    )
+  }
+
+  public async update(ambiente: Ambiente): Promise<void> {
+    const ambienteDoc = await AmbienteModel.findByIdAndUpdate(ambiente.id, {
+      nome: ambiente.nome,
+      tipo: ambiente.tipo,
+      descricao: ambiente.descricao,
+    })
+
+    if (!ambienteDoc) throw new Error('Nenhum ambiente com esse Id foi encontrado.')
+  }
+
+  public async delete(id: string): Promise<void> {
+    const ambienteDoc = await AmbienteModel.findByIdAndDelete(id)
+    if (!ambienteDoc) throw new Error('Nenhum ambiente com esse Id foi encontrado.')
   }
 }

af5f117a90691d08a8f9f86d49a2053bffe9e236
Rafael Rocha <rafaelgus3000@gmail.com>
2025-11-30 20:43:15 -0300

    feat: adiciona novos metodos ao repository de dispositivo

--- src/infra/database/dispositivo/dispositivo.repository.ts
+++ src/infra/database/dispositivo/dispositivo.repository.ts
@@ -1,6 +1,13 @@
 import type { DispositivoRepository } from '@/domain/dispositivo/despositivo.repository.js'
 import { Dispositivo } from '@/domain/dispositivo/dispositivo.entity.js'
+import { AmbienteModel } from '../ambiente/ambiente.model.js'
 import { DispositivoModel } from './dispositivo.model.js'
+
+type DispositivoMongo = {
+  _id: string
+  nome: string
+  ambienteId: string
+}
 
 export class MongooseDispositivoRepository implements DispositivoRepository {
   private constructor() {}
@@ -10,20 +17,35 @@ export class MongooseDispositivoRepository implements DispositivoRepository {
   }
 
   public async save(dispositivo: Dispositivo): Promise<void> {
-    const data = {
+    await DispositivoModel.create({
       _id: dispositivo.id,
       nome: dispositivo.nome,
       ambienteId: dispositivo.ambienteId,
-    }
-
-    const dispositivoDoc = new DispositivoModel(data)
-    await dispositivoDoc.save()
+    })
   }
 
   public async findAll(): Promise<Dispositivo[]> {
-    const dispositivosDoc = await DispositivoModel.find()
-    return dispositivosDoc.map((doc) =>
-      Dispositivo.create(doc.id, doc.nome || undefined, doc.ambienteId || undefined),
-    )
+    const dispositivosDoc = await DispositivoModel.find().lean<DispositivoMongo[]>()
+    return dispositivosDoc.map((doc) => Dispositivo.create(doc._id, doc.nome, doc.ambienteId))
+  }
+
+  public async findById(id: string): Promise<Dispositivo> {
+    const dispositivoDoc = await AmbienteModel.findById(id).lean<DispositivoMongo>()
+    if (!dispositivoDoc) throw new Error('Nenhum dispositivo com esse Id encontrado')
+    return Dispositivo.create(dispositivoDoc._id, dispositivoDoc.nome, dispositivoDoc.ambienteId)
+  }
+
+  public async update(dispositivo: Dispositivo): Promise<void> {
+    const dispositivoDoc = await DispositivoModel.findByIdAndUpdate(dispositivo.id, {
+      nome: dispositivo.nome,
+      ambienteId: dispositivo.ambienteId,
+    })
+
+    if (!dispositivoDoc) throw new Error('Nenhum dispositivo com esse Id encontrado')
+  }
+
+  public async delete(id: string): Promise<void> {
+    const dispositivoDoc = await DispositivoModel.findByIdAndDelete(id)
+    if (!dispositivoDoc) throw new Error('Nenhum dispositivo com esse Id encontrado')
   }
 }

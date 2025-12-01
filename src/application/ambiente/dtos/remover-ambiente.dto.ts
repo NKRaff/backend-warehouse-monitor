817cd0545d72a910f17d56669a0e9ccb9d00e755
Rafael Rocha <rafaelgus3000@gmail.com>
2025-12-01 15:16:44 -0300

    feat: adiciona use case para remoção de ambiente

---
+++ src/application/ambiente/dtos/remover-ambiente.dto.ts
@@ -0,0 +1,5 @@
+export type RemoverAmbienteInputDto = {
+  id: string
+}
+
+export type RemoverAmbienteOutputDto = void
---
+++ src/application/ambiente/use-cases/remover-ambiente.usecase.ts
@@ -0,0 +1,28 @@
+import type { UseCase } from '@/application/usecase.js'
+import type { AmbienteRepository } from '@/domain/ambiente/ambiente.repository.js'
+import type { DispositivoRepository } from '@/domain/dispositivo/despositivo.repository.js'
+import type {
+  RemoverAmbienteInputDto,
+  RemoverAmbienteOutputDto,
+} from '../dtos/remover-ambiente.dto.js'
+
+export class RemoverAmbienteUseCase
+  implements UseCase<RemoverAmbienteInputDto, RemoverAmbienteOutputDto>
+{
+  private constructor(
+    private readonly ambienteRepo: AmbienteRepository,
+    private readonly dispositivoRepo: DispositivoRepository,
+  ) {}
+
+  public static create(ambienteRepo: AmbienteRepository, dispositivoRepo: DispositivoRepository) {
+    return new RemoverAmbienteUseCase(ambienteRepo, dispositivoRepo)
+  }
+
+  public async execute(input: RemoverAmbienteInputDto) {
+    await this.ambienteRepo.delete(input.id)
+    const dispositivos = await this.dispositivoRepo.findByAmbienteId(input.id)
+    if (dispositivos) {
+      dispositivos.map((dis) => dis.removerAmbiente())
+    }
+  }
+}

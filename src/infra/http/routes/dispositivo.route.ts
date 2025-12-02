83e4715fbdc36d4577e120943ccbea10936d1a3f
Rafael Rocha <rafaelgus3000@gmail.com>
2025-12-01 23:52:17 -0300

    feat: adiciona rota http para listagem de dispositivos

--- src/infra/http/routes/dispositivo.route.ts
+++ src/infra/http/routes/dispositivo.route.ts
@@ -1,4 +1,5 @@
 import type { CadastrarDispositivoController } from '@/interface/dispositivo/cadastrar-dispositivo/cadastrar-dispositivo.controller.js'
+import type { ListarDispositivosController } from '@/interface/dispositivo/listar-dispositivos/listar-dispositivos.controller.js'
 import { Router } from 'express'
 
 export class DispositivoRoutes {
@@ -6,13 +7,17 @@ export class DispositivoRoutes {
 
   private constructor(
     private readonly cadastrarDispositivoController: CadastrarDispositivoController,
+    private readonly listarDispositivoController: ListarDispositivosController,
   ) {
     this.routes = Router()
     this.setupRoutes()
   }
 
-  public static create(cadastrarDispositivoController: CadastrarDispositivoController) {
-    return new DispositivoRoutes(cadastrarDispositivoController)
+  public static create(
+    cadastrarDispositivoController: CadastrarDispositivoController,
+    listarDispositivoController: ListarDispositivosController,
+  ) {
+    return new DispositivoRoutes(cadastrarDispositivoController, listarDispositivoController)
   }
 
   private setupRoutes() {
@@ -20,6 +25,15 @@ export class DispositivoRoutes {
       try {
         const result = await this.cadastrarDispositivoController.handle(req.body)
         res.status(201).json(result)
+      } catch (error) {
+        res.status(400).json(error)
+      }
+    })
+
+    this.routes.get('/', async (_req, res) => {
+      try {
+        const result = await this.listarDispositivoController.handle()
+        res.status(200).json(result)
       } catch (error) {
         res.status(400).json(error)
       }

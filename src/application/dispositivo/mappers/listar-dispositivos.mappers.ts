0a67121b4cff5a3f90fd84a0db2923fed201dc8d
Rafael Rocha <rafaelgus3000@gmail.com>
2025-12-01 23:42:40 -0300

    feat: adiciona use case para listagem de dispositivos

---
+++ src/application/dispositivo/mappers/listar-dispositivos.mappers.ts
@@ -0,0 +1,15 @@
+import type { Dispositivo } from '@/domain/dispositivo/dispositivo.entity.js'
+
+export namespace ListarDispositivosMapper {
+  export function paraOutput(dispositivos: Dispositivo[]) {
+    return {
+      dispositivos: dispositivos.map((dispositivo) => {
+        return {
+          id: dispositivo.id,
+          nome: dispositivo.nome,
+          ambienteId: dispositivo.ambienteId,
+        }
+      }),
+    }
+  }
+}

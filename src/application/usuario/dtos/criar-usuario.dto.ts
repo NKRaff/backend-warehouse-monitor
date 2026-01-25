e08b7be5ed5ae1e76bfc8d1b3f5ade781d9cb7d5
Rafael Rocha <rafaelgus3000@gmail.com>
2026-01-24 22:56:24 -0300

    feat: cria autenticação quando o usuario é criado

--- src/application/usuario/dtos/criar-usuario.dto.ts
+++ src/application/usuario/dtos/criar-usuario.dto.ts
@@ -1,6 +1,7 @@
 export type CriarUsuarioInputDto = {
   nome: string
   email: string
+  senha: string
 }
 
 export type CriarUsuarioOutputDto = {
--- src/interface/usuario/criar-usuario/criar-usuario.schema.ts
+++ src/interface/usuario/criar-usuario/criar-usuario.schema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const CriarUsuarioSchema = z.object({
   nome: z.string().min(4),
   email: z.email(),
+  senha: z.string().min(8),
 })
 
 export type CriarUsuarioInputDto = z.infer<typeof CriarUsuarioSchema>

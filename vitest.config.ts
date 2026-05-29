bdc91a73f1570a7275341dc7ff59432e62ee346c
Rafael Rocha <rafaelgus3000@gmail.com>
2026-05-29 14:57:11 -0300

    test: adiciona limite de cobertura e script para testes e2e

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
       provider: 'v8',
       include: ['src/domain/**/*.ts', 'src/application/**/*.ts'],
       exclude: ['src/main.ts', '**/*.dto.ts', '**/*.repository.ts'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+        perFile: true,
+      },
     },
     projects: [
       {
@@ -20,6 +27,12 @@ export default defineConfig({
         test: {
           name: 'integration',
           include: ['./test/**/*.test.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          include: ['./test/**/*.e2e-test.ts'],
         },
       },
     ],

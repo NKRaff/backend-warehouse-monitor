272c141d2175b2c04bcaad477e436a70950da83f
Rafael Rocha <rafaelgus3000@gmail.com>
2026-05-30 12:19:35 -0300

    build: adiciona e configura o vitest

---
+++ vitest.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      include: ['src/domain/**/*.ts', 'src/application/**/*.ts'],
+      exclude: ['src/main.ts', '**/*.dto.ts', '**/*.repository.ts'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+        perFile: true,
+      },
+    },
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['./src/**/*.test.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'integration',
+          include: ['./test/**/*.test.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          include: ['./test/**/*.e2e-test.ts'],
+        },
+      },
+    ],
+  },
+})
